e610d811b50c6cfa3225efd53c3f9c499d8a54d9
alelievr <alelievr@student.42.fr>
2017-12-02 18:00:47 +0100

    added city asset

---
+++ Assets/Scripts/RagdollController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+	Collider[]			ragdollColliders;
+	Rigidbody[]			ragdollRigidbodies;
+	CharacterJoint[]	ragdollJoints;
+
+	// Use this for initialization
+	void Start ()
+	{
+		ragdollColliders = GetComponentsInChildren< Collider >();
+		ragdollRigidbodies = GetComponentsInChildren< Rigidbody >();
+		ragdollJoints = GetComponentsInChildren< CharacterJoint >();
+	}
+
+	public void ActivateRagdoll()
+	{
+
+	}
+}
