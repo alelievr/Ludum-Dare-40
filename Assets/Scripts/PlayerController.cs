fc2aa1018789ce5937572943b1e73a049736ab28
Babacar CISS <bciss@e1r12p1.42.fr>
2017-12-03 20:23:14 +0100

    -_-

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	public	float			toxicity = 50f;
+	public	Slider			toxiSlider;
+
+	// private static System.Random rnd = new System.Random();
+	private	Rigidbody	rb;
+
+	void Start () {
+		InvokeRepeating("ToxicityUpdate", 2f, 0.2f);
+	}
+	
+	void ToxicityUpdate() {
+		toxicity -= 0.5f;
+		if (toxicity < 0) {
+			toxicity = 0;
+		}
+		toxiSlider.GetComponent<Slider>().value = toxicity;
+    }
+
+	void addToxicity(float i) {
+		toxicity += i;
+		if (toxicity > 100f) {
+			toxicity = 100f;
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log(other.gameObject.tag);
+		if (other.gameObject.tag == "cafe") {
+			addToxicity(10f);
+			GameObject.Destroy(other.gameObject);
+		}
+	}
+
+	/*static void randomize (List<string> list) {
+		int n = list.Count;
+        while (n > 1) {
+            int k = (rnd.Next(0, n) % n);
+            n--;
+            string value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+		int i = 0;
+		while (i < list.Count) {
+			Debug.Log(i + ": " + list[i]);
+			i++;
+		}
+	}*/
+}
---
+++ Assets/Scripts/Speedy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speedy : MonoBehaviour {
+
+	public	PlayerController	Player;
+	PlayerAPI	api;
+	private	float				speedW;
+	private	float				speedR;
+
+	// Use this for initialization
+	void Start () {
+		api = GetComponent< PlayerAPI >();
+		speedW = api.defaultPlayerWalkSpeed;
+		speedR = api.defaultPlayerRunSpeed;
+	}
+
+	void Update() {
+		api.SetWalkSpeed(speedW + (Player.toxicity / 100));
+		api.SetRunSpeed(speedR + (Player.toxicity / 100));
+	}
+}
