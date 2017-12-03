3b934643c21cba8518111c745da2778fe8862811
Gabriel FOURNIER <gabfou95@gmail.com>
2017-12-03 14:24:25 +0100

    Fdsf

--- Assets/Scripts/render_with_shader.cs
+++ Assets/Scripts/render_with_shader.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class render_with_shader : MonoBehaviour {
 	public float		intensity;
+	public float 		Intensitytache = 0;
 	public float		zoom = 1;
 	public float		strength;
 	public float		speed;
 	private Material	material;
 	public Texture2D	distortionTexture;
+	public Texture2D	tacheTexture;
 
 
  	// Creates a private material used to the effect
  	void Awake ()
  	{
-	material = new Material( Shader.Find("Hidden/Distort") );
+		material = new Material( Shader.Find("Hidden/Distort") );
  	}
  
 	// Postprocess the image
@@ -28,10 +30,13 @@ public class render_with_shader : MonoBehaviour {
  		}
 
 		material.SetTexture("_DisplacementTex", distortionTexture);
+		material.SetTexture("_TacheTex", tacheTexture);
  		material.SetFloat("_bwBlend", intensity);
 		material.SetFloat("_Strength", strength);
 		material.SetFloat("_Speed", speed);
 		material.SetFloat("_Zoom", zoom);
+		material.SetFloat("_Intensitytache", Intensitytache);
+		material.SetVector("_CentrePoint", new Vector4(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z, 0));
  		Graphics.Blit (source, destination, material);
 	}
 }
