d861c2d693a748fed1b5bc91fa8d3d7bbd9454ea
alelievr <alelievr@student.42.fr>
2017-12-04 21:29:27 +0100

    added music mixer

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+	public List< Vector3 >	list = new List< Vector3 >();
+	public float			speed = 1f;
+	private int				index;
+
+	public bool		canMove = true;
+	// Use this for initialization
+	void Start () {
+		index = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!canMove)
+			return ;
+		
+		if ((list [index] - transform.position).magnitude > 0.01)
+			transform.position = Vector3.MoveTowards (transform.position, list [index], speed * Time.deltaTime);
+		else if (index < list.Count - 1)
+			index++;
+		else
+			index = 0;
+	}
+}
---
+++ Assets/Scripts/ScalingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScalingPlatform : MonoBehaviour
+{
+	public float	duration = 2;
+	public Ease		ease;
+	public Vector3	scale;
+
+	void Start ()
+	{
+		transform.DOScale(Vector3.one / 2, duration).SetLoops(-1).SetEase(ease);
+	}
+	void Update () {
+		
+	}
+}
